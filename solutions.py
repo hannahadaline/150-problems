e9b8790ca6da1a36de865457ccfd99c5bbec284a
Hannah <hannahadalinesun@gmail.com>
2025-07-14 13:19:50 -0400

    7 through 11

--- solutions.py
+++ solutions.py
@@ -1,5 +1,115 @@
+#* 7 (1.1)
+''' Given two strings, return whether the two strings are anagrams
+(containing the same characters but in a different order '''
 
-#* 14 (1.6)
+def anagrams(s1, s2):
+   counts_1 = {}
+   for char in s1:
+      if char in counts_1:
+         counts_1[char] += 1
+      else:
+         counts_1[char] = 1
+
+   counts_2 = {}
+   for char in s2:
+      if char in counts_2:
+         counts_2[char] += 1
+      else:
+         counts_2[char] = 1
+
+   return counts_1 == counts_2
+         
+def anagrams(s1, s2):
+   counts = {}
+   for char in s1:
+      if char in counts:
+         counts[char] += 1
+      else:
+         counts[char] = 1
+
+   for char in s2:
+      if char in counts:
+         counts[char] -= 1
+      else:
+         return False
+
+   for char in counts:
+      if counts[char] != 0:
+         return False
+   return True
+
+
+
+#* 8 (1.2)
+''' Given a string, return most frequent char in the string
+    If there are ties, return first char to occur '''
+
+# using >= (the equals part) and then reversing the string 
+# gives the first occurrence in one pass 
+
+def most_frequent_char(s):
+    freqs = {}
+    max_freq = 0
+    max_char = ""
+   
+    for i in range(len(s) - 1, -1, -1):
+        char = s[i]
+        freqs[char] = freqs.get(char, 0) + 1
+
+        if freqs[char] >= max_freq:
+            max_freq = freqs[char]
+            max_char = char
+
+    return max_char
+
+
+#* 9 (1.3)
+''' Given a list of integers and a target sum, 
+    return a tuple (in any order) of the two indices whose values sum to the target
+    There is guaranteed to be one solution '''
+
+def pair_sum(numbers, target_sum):
+    prev = {}
+    for i, num in enumerate(numbers):
+        complement = target_sum - num
+        if complement in prev:
+            return (prev[complement], i)
+        else:
+            prev[num] = i
+
+
+#* 10 (1.4)
+''' Given a list of integers and a target product, 
+    return a tuple (in any order) of the two indices whose values multiply to the target
+    There is guaranteed to be one solution '''
+
+def pair_product(numbers, target_product):
+    prev_nums = {}
+    for i, num in enumerate(numbers):
+        complement = target_product / num
+        if complement in prev_nums:
+            return (prev_nums[complement], i)
+
+        prev_nums[num] = i
+
+
+#* 11 (1.5) 
+''' Given two input lists, each without duplicates,
+    return a list containing elements in both lists '''
+
+def intersection(a, b):
+    a_set = set(a)
+    result = [ element for element in b if element in a_set ]
+    
+    # for element in b:
+    #     if element in a_set:
+    #         result.append(element)
+
+    return result
+
+
+
+#* 12 (1.6)
 ''' Given two lists a and b, each of which contains unique items,
     return a list of all items that are in only one of the two sets but not both '''
 
@@ -36,7 +146,8 @@ set_c.update(set_b) # how to add all elements from one set into another
 print(set_c)
 
 
-#* 15 (1.7)
+
+#* 13 (1.7)
 ''' Given a list of items, return whether all items in the list are unique '''
 
 def all_unique(items):
@@ -53,7 +164,7 @@ def all_unique(items):
     return len(items) == len(items_set)
 
 
-#* 16 (1.8)
+#* 14 (1.8)
 ''' Given two lists of items, 
     return list of elements common to both lists, 
     as many times as they occur in both lists '''
